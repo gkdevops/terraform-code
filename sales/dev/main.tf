6b7c479e827930bb088a5c834858d2a2c8366560
Goutham Kumar <chgoutam@gmail.com>
2025-01-04 17:24:52 +0000

    Add another way for module decleration

--- sales/dev/main.tf
+++ sales/dev/main.tf
@@ -2,15 +2,15 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.82.2"
     }
   }
 
   backend "s3" {
-    bucket = "terraform-remote-state-prepzee"
-    key    = "dev/sales/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "terraform-remote-state-prepzee"
+    key     = "dev/sales/terraform.tfstate"
+    region  = "us-east-1"
     profile = "development"
   }
 
@@ -23,15 +23,12 @@ provider "aws" {
 }
 
 module "my_s3_bucket" {
-  source            = "../../tf_modules/s3_bucket/"  # Path to the module
-  bucket_name       = "dev-prepzee-12345"
-  acl               = "private"
-  force_destroy     = true
-  enable_versioning = true
-  tags = {
-    Environment = "Dev"
-    Project     = "S3Demo"
-  }
+  source            = "../../tf_modules/s3_bucket/" # Path to the module
+  bucket_name       = var.bucket_name
+  acl               = var.acl
+  force_destroy     = var.force_destroy
+  enable_versioning = var.enable_versioning
+  tags              = var.tags
 }
 
 output "bucket_outputs" {
---
+++ sales/dev/terraform.tfvars
@@ -0,0 +1,8 @@
+bucket_name       = "dev-prepzee-12345"
+acl               = "private"
+force_destroy     = true
+enable_versioning = true
+tags = {
+  Environment = "Dev"
+  Project     = "S3Demo"
+}
