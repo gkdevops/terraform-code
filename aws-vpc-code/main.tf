a725baa07840547110b6c2849a30243cb543c141
Goutham Kumar <47983326+gkdevops@users.noreply.github.com>
2025-05-21 08:05:26 +0530

    Update main.tf

--- aws-vpc-code/main.tf
+++ aws-vpc-code/main.tf
@@ -120,7 +120,7 @@ resource "aws_route_table_association" "private_subnet_assoc" {
 }
 
 # Security Group Creation
-resource "aws_security_group" "test_sg" {
+resource "aws_security_group" "sg_public" {
   name   = "my-test-sg"
   vpc_id = aws_vpc.main.id
 }
@@ -129,7 +129,7 @@ resource "aws_security_group" "test_sg" {
 resource "aws_security_group_rule" "ssh_inbound_access" {
   from_port         = 22
   protocol          = "tcp"
-  security_group_id = aws_security_group.test_sg.id
+  security_group_id = aws_security_group.sg_public.id
   to_port           = 22
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
@@ -138,7 +138,7 @@ resource "aws_security_group_rule" "ssh_inbound_access" {
 resource "aws_security_group_rule" "http_inbound_access" {
   from_port         = 80
   protocol          = "tcp"
-  security_group_id = aws_security_group.test_sg.id
+  security_group_id = aws_security_group.sg_public.id
   to_port           = 80
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
@@ -148,7 +148,7 @@ resource "aws_security_group_rule" "http_inbound_access" {
 resource "aws_security_group_rule" "all_outbound_access" {
   from_port         = 0
   protocol          = "-1"
-  security_group_id = aws_security_group.test_sg.id
+  security_group_id = aws_security_group.sg_public.id
   to_port           = 0
   type              = "egress"
   cidr_blocks       = ["0.0.0.0/0"]
